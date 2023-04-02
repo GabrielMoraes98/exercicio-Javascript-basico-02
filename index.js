e749b467ed6d48b6394d83437d85683b0873e506
Gabriel Ferreira <gabrielmoraes2507@outlook.com>
2023-04-02 18:59:53 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+ const Resultado = confirm('Seja bem vindo (a), aventureiro(a)! Gostaria de pagar R$50,00 para passar a noite na nossa Hospedagem?')
+
+if(Resultado){ //if(resutaldo === True) é a mesma coisa
+    alert( 'Ótimo! Nós temos as melhores camas da Região')
+}else{
+    alert('Que pena! Você parecia ser uma pessoa mais legal')
+}
